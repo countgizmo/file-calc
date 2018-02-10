51a76aa7813058333c6607b17a6b324a829dbc7a
countgizmo <count.gizmo@gmail.com>
2018-02-10 10:09:30 +0700

    Calculation passes the first test
    Didn't have a lot of small commits between my failing test and passing test. It was too short to commit. I've spent most time wrestling with Clojure math stuff since I don't have that much experience here and need to google funcitons every time.
    
    But here's what was happening in my brain (censored):
    1. I wanted to compose everything into one big function and then run it.
    2. Then I thought about how would I pass input number...
    3. And then I thought: ok, we have a collection of things and we must get one number in the end. Means - reducing! Reduce. So I've decided to switch stuff around, use calculation functions as a collection of stuff and run them starting with 0, applyting partial result to the next funciton.
    4. So I've spent some time massaging strings into partially applied functions that we can later call on partial result in reduce function.

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject file-calc "0.1.0"
   :description "Test assignment for Zimpler"
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :source-paths ["src"]
+  :test-paths ["test"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [proto-repl "0.3.1"]])
--- src/file_calc/core.clj
+++ src/file_calc/core.clj
@@ -1,5 +1,27 @@
-(ns file-calc.core)
+(ns file-calc.core
+  (:require [clojure.string :as str]))
+
+(def operations-fns
+  {"ADD" +
+   "SUB" -
+   "MUL" *
+   "DIV" /
+   "SQR" (partial #(Math/pow % 2))})
+
+(defn str->op-arg
+ [s]
+ (str/split s #" " 2))
+
+(defn op-arg->fn
+ [[op-str arg-str]]
+ (let [f (get operations-fns op-str)]
+   (if (= "SQR" op-str) f
+     (partial f (Integer. arg-str)))))
+
+(def str->fn (comp op-arg->fn str->op-arg))
 
 (defn calculate
  [coll]
- 0)
+ (->> (map str->fn coll)
+      (reduce (fn [res op] (op res)) 0)
+      (Math/round)))
